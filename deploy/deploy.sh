d3f062805256e920627eb0f55d8001f5bf197bf1
Peng Wei <pengwei715@gmail.com>
2025-01-27 21:42:40 -0800

    adding the deploy instance

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -15,9 +15,9 @@
 SUBSCRIPTION_ID="${SUBSCRIPTION_ID:-f21c56c2-6802-4722-a757-2785b91b19a9}"
 RESOURCE_GROUP="${RESOURCE_GROUP:-ml_workspace_east_us_2}"
 WORKSPACE_NAME="${WORKSPACE_NAME:-ml_workspace_east_us_2}"
-ENDPOINT_NAME="${ENDPOINT_NAME:-ambient-audio-endpoint}"
+ENDPOINT_NAME="${ENDPOINT_NAME:-ambient-audio-realtime-endpoint}"
 ACR_NAME="${ACR_NAME:-dev85ec8a04}"
-IMAGE_NAME="${IMAGE_NAME:-ambient_audio}"
+IMAGE_NAME="${IMAGE_NAME:-ambient_audio_realtime}"
 IMAGE_TAG="${IMAGE_TAG:-0.1}"
 INSTANCE_TYPE="${INSTANCE_TYPE:-Standard_NC4as_T4_v3}"
 
